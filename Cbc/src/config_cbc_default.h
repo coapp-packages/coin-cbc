471426889518946b36d76a7b56238f35c12ef86a
PetterS <petter.strandmark@gmail.com>
2011-07-13 11:26:24 +0200

    Initial commit

---
+++ Cbc/src/config_cbc_default.h
@@ -0,0 +1,19 @@
+
+/***************************************************************************/
+/*           HERE DEFINE THE PROJECT SPECIFIC PUBLIC MACROS                */
+/*    These are only in effect in a setting that doesn't use configure     */
+/***************************************************************************/
+
+/* Version number of project */
+#define CBC_VERSION        "2.7"
+
+/* Major Version number of project */
+#define CBC_VERSION_MAJOR      2
+
+/* Minor Version number of project */
+#define CBC_VERSION_MINOR      7
+
+/* Release Version number of project */
+#define CBC_VERSION_RELEASE 9999
+
+#define COIN_HAS_CLP 1
